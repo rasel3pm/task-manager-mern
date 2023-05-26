d3e1a34d1051e01276422c61432d3957a719efdc
Rasel <mdraselkibria11@gmail.com>
2023-05-26 17:25:31 +0600

    client-side delete route set but not work

--- client-side/src/components/Task-card.js
+++ client-side/src/components/Task-card.js
@@ -1,6 +1,18 @@
 import React from "react";
 import {Button, Card,ButtonGroup} from "react-bootstrap";
-const TaskCard = ({title,description,author})=>{
+import axios from "axios";
+import {toast,ToastContainer} from "react-toastify";
+const TaskCard = ({author,title,description})=>{
+    const handleSubmit = (_id) => {
+        axios
+            .delete(`http://localhost:5000/api/v1//delete/${_id}`)
+            .then((res) => {
+                toast.success(`${res.data.message}`);
+            })
+            .catch((err) => {
+                toast.warning(`${err}`)
+            });
+    };
     return(
         <Card border="primary" style={{ width: 'auto', }}>
             <Card.Header>Author : {author}</Card.Header>
@@ -14,9 +26,10 @@ const TaskCard = ({title,description,author})=>{
                 <ButtonGroup aria-label="Basic example">
                     <Button variant="secondary">View</Button>
                     <Button variant="secondary">Edit</Button>
-                    <Button variant="danger">Delete</Button>
+                    <Button variant="danger" onClick={handleSubmit}>Delete</Button>
                 </ButtonGroup>
             </Card.Footer>
+            <ToastContainer/>
         </Card>
     )
 }
