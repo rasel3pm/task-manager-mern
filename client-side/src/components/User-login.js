6621e667b1a6de463c1821b2baf7210f9d96a63a
Rasel <mdraselkibria11@gmail.com>
2023-05-26 12:56:57 +0600

    so solve

--- client-side/src/components/User-login.js
+++ client-side/src/components/User-login.js
@@ -29,7 +29,7 @@ const UserLogin = ()=>{
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <Form.Text className="text-muted">
-                  {error}
+                  Error :jhkhjkhjk
                 </Form.Text>
             </Form.Group>
 
